5ee86b6b30d3f49acad0b1ee49339890d7c45cd3
nehabhardwaj3001 <bhardwajneha3001@gmail.com>
2024-04-09 11:00:41 +0530

    added toastify

--- src/App.js
+++ src/App.js
@@ -1,58 +1,103 @@
-import React, {useState, useEffect} from 'react'; 
-import './App.css';
-import { Button } from '@material-ui/core';
-import { TextField } from '@material-ui/core';
-import { useDispatch, useSelector } from 'react-redux';
-import * as types from './Redux/actions';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Button } from "@material-ui/core";
+import { TextField } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import * as types from "./Redux/actions";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState(" ");
-  const state  = useSelector(state => state.data);
+  const state = useSelector((state) => state.data);
+  const [attempted, setAttempted] = useState(false);
 
   const updateSearch = () => {
     setQuery(search);
     setSearch("");
-  }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateSearch();
+  };
   
   let dispatch = useDispatch();
-
+  
   useEffect(() => {
-    dispatch({ type: types.FETCH_WEATHER_START, payload: query })
+    // Only make the API call if query is not empty
+    if (query.trim() !== '') {
+      dispatch({ type: types.FETCH_WEATHER_START, payload: query });
+      setAttempted(true); // Set attempted to true when API call is made
+    }
   }, [query]);
 
-  console.log("state...", state, state?.data)
+  useEffect(() => {
+    if (attempted && state?.data && state?.data?.cod === '404') { // Check if attempted is true before showing error
+      toast.error('City not found. Please enter a valid city name.');
+    }
+  }, [attempted, state.data]);
 
-  const className = !state?.data?.name ? "data" : state?.data?.main?.temp_max-273 > 30 ? "App_hot" : "App_cold";
+
+  const className = !state?.data?.name
+    ? "data"
+    : state?.data?.main?.temp_max - 273 > 30
+    ? "App_hot"
+    : "App_cold";
 
   return (
     <div>
-    <div  className={`common ${className}`}>
-      <h2 className='header'> Weather App </h2>
-      <form noValidate autoComplete='off'>
-        <TextField className='textfield'
-          id="outlined-basic"
-          label="city"
-          variant="outlined"
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)} />
-        <Button variant="contained"
-          color='Primary'
-          style={{ width: '80px', height: '50px' }}
-          onClick={updateSearch}>
-          Search
-        </Button>
-      </form>
-     {state?.data?.name ?
-      (
-     <div className={state?.data?.main?.temp_max-273 > 30 ? "data_hot" : "data_cold"}>
-          <h1>{state?.data?.name}</h1>
-          <h2><span>Max-Temp:- </span>{(state?.data?.main?.temp_max-273)?.toFixed(2)}°C</h2>
-          <h2><span>Min-Temp:- </span>{(state?.data?.main?.temp_min-273)?.toFixed(2)}°C</h2>
-          <h2><span>{state?.data?.weather[0]?.main}</span></h2>
-        </div>) : <h1></h1>}
-    </div>
+      <ToastContainer />
+      <div className={`common ${className}`}>
+        <h2 className="header"> Weather App </h2>
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <TextField
+            className="textfield"
+            id="outlined-basic"
+            label="city"
+            variant="outlined"
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="Primary"
+            style={{ width: "80px", height: "50px", marginLeft: "20px" }}
+            type="submit"
+          >
+            Search
+          </Button>
+        </form>
+        {state?.data?.name ? (
+          <div
+            className={
+              state?.data?.main?.temp_max - 273 > 30 ? "data_hot" : "data_cold"
+            }
+          >
+            <h1>{state?.data?.name}</h1>
+            <h2>
+              <span>Temperature:- </span>
+              {(state?.data?.main?.temp - 273)?.toFixed(2)}°C
+            </h2>
+            <h2>
+              <span>Humidity:- </span>
+              {state?.data?.main.humidity} %
+            </h2>
+            <h2>
+              <span>Wind Speed:- </span>
+              {state?.data?.wind?.speed} meter/sec
+            </h2>
+            <h2>
+              <span>Weather Condition:- </span>
+              {state?.data?.weather[0].description}
+            </h2>
+          </div>
+        ) : (
+          <h1></h1>
+        )}
+      </div>
     </div>
   );
 }
--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const getWeather = async(query)=>{
     // const url=`https://api.openweathermap.org/data/2.5/weather?q={query}&appid=89f551cc72931ac0642c29c0e0db8885`;
@@ -7,10 +9,14 @@ export const getWeather = async(query)=>{
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=89f551cc72931ac0642c29c0e0db8885`);
         const data= await response.json();
+        // if(data.cod !== 200) {
+        //     toast.error("Failed to fetch weather data. Please try again later.", data.message);
+        // }
         console.log("data", data)
         return data;
     } catch (e){
-        console.log(e);
+        // toast.error("Failed to fetch weather data. Please try again later.", e);
+        console.log("Failed to fetch weather data. Please try again later.", e);
     }
     
 }
